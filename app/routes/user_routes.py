7088b82ba966557d0705d095e8bfd1fed4d0f487
Michał Kuszewski <michal.kuszewski09@gmail.com>
2025-08-16 10:54:01 +0200

    Move error function to utils file

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -7,6 +7,7 @@ from pydantic import ValidationError
 from ..dtos import UserCreateDTO, UserUpdateDTO, UserReadDTO
 from ..exceptions import EntityNotFoundException, EntityPersistenceException
 from ..services import user_service
+from ..utils import create_error_response
 
 user_blueprint = Blueprint("users", __name__)
 
@@ -25,7 +26,7 @@ def get_user(user_id: int) -> tuple[Response, HTTPStatus]:
 
         return jsonify(user.model_dump()), HTTPStatus.OK
     except EntityNotFoundException as e:
-        return jsonify({"error": str(e)}), HTTPStatus.NOT_FOUND
+        return create_error_response(str(e)), HTTPStatus.NOT_FOUND
 
 
 @user_blueprint.route("/", methods=["POST"])
@@ -34,16 +35,16 @@ def create_user() -> tuple[Response, HTTPStatus]:
         payload: Optional[dict] = request.get_json()
 
         if payload is None:
-            return jsonify({"error", "Missing JSON body"}), HTTPStatus.BAD_REQUEST
+            return create_error_response("Missing JSON body"), HTTPStatus.BAD_REQUEST
 
         user_create_dto: UserCreateDTO = UserCreateDTO(**payload)
         user_read_dto: UserReadDTO = user_service.create_user(user_create_dto)
 
         return jsonify(user_read_dto.model_dump()), HTTPStatus.CREATED
     except ValidationError as e:
-        return jsonify({"error": str(e.errors())}), HTTPStatus.BAD_REQUEST
+        return create_error_response(str(e.errors())), HTTPStatus.BAD_REQUEST
     except EntityPersistenceException as e:
-        return jsonify({"error": str(e)}), HTTPStatus.BAD_REQUEST
+        return create_error_response(str(e)), HTTPStatus.BAD_REQUEST
 
 
 @user_blueprint.route("/<int:user_id>", methods=["PUT"])
@@ -52,15 +53,15 @@ def update_user(user_id: int) -> tuple[Response, HTTPStatus]:
         payload: Optional[dict] = request.get_json()
 
         if payload is None:
-            return jsonify({"error", "Missing JSON body"}), HTTPStatus.BAD_REQUEST
+            return create_error_response("Missing JSON body"), HTTPStatus.BAD_REQUEST
 
         user_update_dto: UserUpdateDTO = UserUpdateDTO(**payload)
         user_read_dto: UserReadDTO = user_service.update_user(user_id, user_update_dto)
 
         return jsonify(user_read_dto.model_dump()), HTTPStatus.OK
     except EntityNotFoundException as e:
-        return jsonify({"error": str(e)}), HTTPStatus.NOT_FOUND
+        return create_error_response(str(e)), HTTPStatus.NOT_FOUND
     except ValidationError as e:
-        return jsonify({"error": str(e.errors())}), HTTPStatus.BAD_REQUEST
+        return create_error_response(str(e.errors())), HTTPStatus.BAD_REQUEST
     except EntityPersistenceException as e:
-        return jsonify({"error": str(e)}), HTTPStatus.BAD_REQUEST
+        return create_error_response(str(e)), HTTPStatus.BAD_REQUEST
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,11 @@
 from datetime import datetime, timezone
 
+from flask import Response, jsonify
+
 
 def get_utc_time() -> datetime:
     return datetime.now(timezone.utc)
+
+
+def create_error_response(message: str) -> Response:
+    return jsonify({"error": message})
